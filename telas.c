c2bdda36255e1bc79507322efeceeebd6d5f0a15
Laetyr <91507446+Laetyr@users.noreply.github.com>
2022-08-28 16:24:35 -0300

    adicao da tela de orientacoes

--- telas.c
+++ telas.c
@@ -18,3 +18,22 @@ char tela_principal(void) {
     scanf("%c", &resposta);
     return resposta;
 }
+
+char tela_orientacoes(void) {
+    char resposta;
+    printf("\n");
+    printf("###################################################################################################\n");
+    printf("#                                            Orientacoes                                          #\n");
+    printf("###################################################################################################\n");
+    printf("# No menu clientes ira realizar o cadastro, encontrar contato e exlusao dos clientes.             #\n");
+    printf("# No menu roupas e fantasias irá fazer o aluguel, ver os que foram alugados e por quanto tempo.   #\n");
+    printf("# No menu locacoes encontrara os valores do aluguel e por quem foi alugado.                       #\n");
+    printf("# No menu relatorio esta o historico dos alugueis e a quantidade de vezes que cada cliente alugou.#\n");
+    printf("# No Menu sobre, tem as descricoes de cada fantasia ou roupa assim como as opcoes disponiveis.    #\n");
+    printf("# No menu orientacoes descreve oque cada opcao faz                                                #\n");
+    printf("#                                       0 - Menu Principal                                        #\n");
+    printf("###################################################################################################\n\n");
+    printf("Escolha sua opcao: ");
+    scanf("%c", &resposta);
+    return resposta;
+}
